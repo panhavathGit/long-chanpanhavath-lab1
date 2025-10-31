d59e9d6adb71c61fd62e1e8e8f1ad44ff2eb8abe
Chanpanhavath <chanpanhavath@gmail.com>
2025-10-31 10:19:53 +0700

    run second test

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello, GitHub Actions!');
     res.send('hi test ');
+    res.send('hi test second time ');
 });
 module.exports = app;
